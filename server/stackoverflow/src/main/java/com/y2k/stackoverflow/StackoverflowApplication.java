552d5762c1aec1d92953d30f5a6dedaeac49c3a5
phant <phantom4060@gmail.com>
2022-12-30 15:36:37 +0900

    test

--- server/stackoverflow/src/main/java/com/y2k/stackoverflow/StackoverflowApplication.java
+++ server/stackoverflow/src/main/java/com/y2k/stackoverflow/StackoverflowApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class StackoverflowApplication { //test
+public class StackoverflowApplication { //test//2
 
 	public static void main(String[] args) {
 		SpringApplication.run(StackoverflowApplication.class, args);
